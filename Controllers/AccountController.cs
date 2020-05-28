b519d6668d29d34f6eaf25af92571e80ff84217b
Siddharth <siddharthvaibhav.8@gmail.com>
2020-05-28 15:28:27 +0530

    Added Local Account to External Logins

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -71,8 +71,50 @@ namespace EmployeeRegistrationApp.Controllers
         }
 
         [HttpGet]
-        public IActionResult ChangePassword()
+        public async Task<IActionResult> AddPassword()
         {
+            var user = await userManager.GetUserAsync(User);
+            var userHasPassword = await userManager.HasPasswordAsync(user);
+            if (userHasPassword)
+            {
+                return RedirectToAction("ChangePassword");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPassword(AddPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user =await userManager.GetUserAsync(User);
+                var result = await userManager.AddPasswordAsync(user, model.NewPassword);
+                if (!result.Succeeded)
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                        return View();
+                    }
+
+                }
+
+                await signInManager.RefreshSignInAsync(user);
+                return View("AddPasswordConfirm");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ChangePassword()
+        {
+            var user =await userManager.GetUserAsync(User);
+            var userHasPassword= await userManager.HasPasswordAsync(user);
+            if (!userHasPassword)
+            {
+                return RedirectToAction("AddPassword");
+            }
             return View();
         }
 
---
+++ ViewModels/AddPasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegistrationApp.ViewModels
+{
+    public class AddPasswordViewModel
+    {
+        [Required]
+        [Display(Name = "New Password")]
+        [DataType(DataType.Password)]
+
+        public string NewPassword { get; set; }
+
+        [Display(Name ="Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage ="New Password and Confirm Password does not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
